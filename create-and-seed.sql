f05699cf1e3abde6691c4b35924cd4bf9fef2369
Solea Miller <soleamiller@gmail.com>
2026-04-06 14:18:14 -0400

    complete

--- create-and-seed.sql
+++ create-and-seed.sql
@@ -47,10 +47,20 @@ CREATE TABLE products (
     product_type TEXT NOT NULL,
     expiration_date DATE NOT NULL,
     price NUMERIC(5, 2) NOT NULL,
-
+    in_stock BOOLEAN NOT NULL DEFAULT 't'
 );
 
 
 -- ============================================================
 -- Step 3: Insert seed data (at least 8 rows)
 -- ============================================================
+
+INSERT INTO products (product_name, product_type, expiration_date, price, in_stock) VALUES
+  ('Brown Eggs', 'poultry', '2026-05-01', 10.99, 'f'),
+  ('Moo Moo Meadows Milk', 'dairy', '2026-05-02', 8.99, 't'),
+  ('Bread Loaf', 'baked goods', '2026-05-03', 4.99, 't'),
+  ('Heinz Ketchup', 'condiment', '2026-05-04', 3.99, 't'),
+  ('Pepsi', 'beverage', '2026-05-05', 1.99, 't'),
+  ('Cola', 'beverage', '2026-05-06', 1.99, 't'),
+  ('Doritos', 'snack', '2026-05-07', 2.99, 't'),
+  ('Apple Pie', 'baked goods', '2026-05-08', 14.99, 't');
